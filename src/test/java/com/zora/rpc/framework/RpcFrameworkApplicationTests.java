74cd6de428d12b1797683090e05d37825e1343ed
ZoraZora <zora759gk@gmail.com>
2020-12-30 13:53:02 +0800

    初始化项目提交

---
+++ src/test/java/com/zora/rpc/framework/RpcFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zora.rpc.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
